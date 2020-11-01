0e538fc9ef0516351a4381a29f40537a7676eceb
smahaveersingh <smahaveermadhu@gmail.com>
2020-11-01 01:04:19 +0000

    Intial Upload

---
+++ components/Header.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const Header = props => {
+    return (
+        <View style={styles.Header}>
+            <Text style={styles.HeaderTitle}>{props.title}</Text>
+        </View>
+    );
+
+};
+
+const styles = StyleSheet.create({
+    Header: {
+        width: '100%',
+        height: 90,
+        padding: 36,
+        backgroundColor: '#000000',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    HeaderTitle: {
+        color: 'white',
+        fontSize: 18
+    }
+});
+
+export default Header;
