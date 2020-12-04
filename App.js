7727ab80b91e8ee8a13167800d82fef824e86873
smahaveersingh <smahaveermadhu@gmail.com>
2020-12-04 23:31:37 +0000

    Login page setup

--- App.js
+++ App.js
@@ -3,11 +3,8 @@ import { Button, StyleSheet, View } from 'react-native';
 
 import Header from './components/Header';
 import LocationPicker from './components/LocationPicker';
-import { createDrawerNavigator} from '@react-navigation/drawer';
-import { NavigationContainer } from '@react-navigation/native';
 
-
-function HomeScreen({ navigation }) {
+const App = () => {
   return (
     <View style={styles.screen}>
       <Header title="Find your Car"/>
@@ -17,31 +14,11 @@ function HomeScreen({ navigation }) {
   );
 }
 
-function NotificationsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Button onPress={() => navigation.goBack()} title="Go back home" />
-    </View>
-  );
-}
-
-const Drawer = createDrawerNavigator();
-
-
-
-export default function App() {
-  return (
-      <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Home" component={HomeScreen} />
-        <Drawer.Screen name="Notifications" component={NotificationsScreen} />
-      </Drawer.Navigator>
-      </NavigationContainer>    
-  );
-}
-
 const styles = StyleSheet.create({
   screen: {
     flex: 1
   }
 });
+
+export default App;
+
---
+++ components/AuthScreen.js
@@ -0,0 +1,2 @@
+import React from 'react';
+import { ScrollView, } from 'react-native';
