8b50926401a1566bbeda78b30272c7ead9c8d165
smahaveersingh <smahaveermadhu@gmail.com>
2020-12-05 03:31:43 +0000

    Onboarding screen and Login page setup

--- App.js
+++ App.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import { Button, StyleSheet, View } from 'react-native';
-
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
 import Header from './components/Header';
 import LocationPicker from './components/LocationPicker';
+import RootStackScreen from './components/RootStackScreen';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Drawer = createStackNavigator();
 
 const App = () => {
   return (
-    <View style={styles.screen}>
+    <NavigationContainer>
+    <RootStackScreen />
+    {/* <View style={styles.screen}> {...}
       <Header title="Find your Car"/>
       <Button onPress={() => navigation.toggleDrawer()} title="Open Drawer" />
       <LocationPicker />
-    </View>
+  </View> */}
+  </NavigationContainer>
   );
 }
 
--- components/AuthScreen.js
+++
@@ -1,2 +0,0 @@
-import React from 'react';
-import { ScrollView, } from 'react-native';
